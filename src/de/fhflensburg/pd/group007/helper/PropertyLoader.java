82f141fa07f7238701c75902c0ed3dfbc118b32f
Thomas Peikert <t.s.peikert@gmail.com>
2015-06-23 17:17:11 +0200

    fixed PropertyLoader issue where the returned value would be null if the property key didn't exist

--- src/de/fhflensburg/pd/group007/helper/PropertyLoader.java
+++ src/de/fhflensburg/pd/group007/helper/PropertyLoader.java
@@ -5,10 +5,16 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertyLoader {
+	/**
+	 * gets a property by name from config.properties
+	 * @param property
+	 * @return {String | null} property value
+	 */
 	public static String getProperty(String property) {
 		 
 		Properties prop = new Properties();
 		InputStream input = null;
+		String value = "";
 	 
 		try {
 	 
@@ -18,21 +24,23 @@ public class PropertyLoader {
 			prop.load(input);
 	 
 			// get the property value and print it out
-			return prop.getProperty(property);
+			value = prop.getProperty(property);
+			if (value == null) {
+				value = "";
+			}
 			
 		} catch (IOException ex) {
 			ex.printStackTrace();
+			return value;
 		} finally {
 			if (input != null) {
 				try {
-					input.close();
+					input.close();	
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
 		}
-		
-		return "";
-	 
-	  }
+		return value;
+	}
 }
